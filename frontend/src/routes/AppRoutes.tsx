b86305773040da00f3ada06e060424341a1d7c3a
Daniela Mihaluta <daniela.mihaluta05@gmail.com>
2026-02-16 21:30:49 +0200

    Creare Footer 2

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "../layouts/Navbar";
+import Footer from "../layouts/Footer";
 import Home from "../pages/Home";
 import Adoption from "../pages/Adoption";
 import LostPets from "../pages/LostPets";
@@ -14,18 +15,21 @@ export default function AppRoutes() {
     return (
         <>
             <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/adoptie" element={<Adoption />} />
-                <Route path="/pierdute" element={<LostPets />} />
-                <Route path="/veterinari" element={<Veterinari />} />
-                <Route path="/donatii" element={<Donations />} />
-                <Route path="/sitters" element={<SittersList />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/quiz" element={<Quiz />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
+            <main style={{ flex: 1 }}>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/adoptie" element={<Adoption />} />
+                    <Route path="/pierdute" element={<LostPets />} />
+                    <Route path="/veterinari" element={<Veterinari />} />
+                    <Route path="/donatii" element={<Donations />} />
+                    <Route path="/sitters" element={<SittersList />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/quiz" element={<Quiz />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </main>
+            <Footer />
         </>
     );
 }
