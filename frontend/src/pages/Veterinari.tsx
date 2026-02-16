0fd8d92558e41bc573f80da66f6546ed223b94b3
Daniela Mihaluta <daniela.mihaluta05@gmail.com>
2026-02-16 21:19:02 +0200

    Definire interfață pagina Veterinari

--- frontend/src/pages/Veterinari.tsx
+++ frontend/src/pages/Veterinari.tsx
@@ -1,3 +1,14 @@
+interface Veterinar {
+    id: string;
+    name: string;
+    city: string;
+    address: string;
+    phone: string;
+    services: string[];
+    emergency: boolean;
+    description: string;
+}
+
 export default function Veterinari() {
     return (
         <div>
