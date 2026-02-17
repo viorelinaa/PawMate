0b53a01f9a9131ef63270ecf9224632cd573356f
Daniela Mihaluta <daniela.mihaluta05@gmail.com>
2026-02-17 14:08:03 +0200

    scroll to top

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import AppRoutes from "./routes/AppRoutes";
 import Navbar from "./layouts/Navbar";
+
+function ScrollToTop() {
+    const { pathname } = useLocation();
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+    return null;
+}
 
 export default function App() {
     return (
         <BrowserRouter>
+            <ScrollToTop />
             <Navbar />
             <AppRoutes />
         </BrowserRouter>
