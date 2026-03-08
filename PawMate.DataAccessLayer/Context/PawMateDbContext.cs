6944af885bfe8253907ce22705a03a0ea8c88a0a
Daniela Mihaluta <daniela.mihaluta05@gmail.com>
2026-03-08 10:14:46 +0200

    backend data access layer

---
+++ PawMate.DataAccessLayer/Context/PawMateDbContext.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using PawMate.Domain.Entities.Adoption;
+using PawMate.Domain.Entities.BlogPost;
+using PawMate.Domain.Entities.Event;
+using PawMate.Domain.Entities.LostPet;
+using PawMate.Domain.Entities.Marketplace;
+using PawMate.Domain.Entities.Pet;
+using PawMate.Domain.Entities.User;
+using PawMate.Domain.Entities.Volunteer;
+
+namespace PawMate.DataAccessLayer.Context;
+
+public sealed class PawMateDbContext : DbContext
+{
+    public DbSet<UserEntity> Users { get; set; }
+    public DbSet<PetEntity> Pets { get; set; }
+    public DbSet<AdoptionEntity> Adoptions { get; set; }
+    public DbSet<LostPetEntity> LostPets { get; set; }
+    public DbSet<EventEntity> Events { get; set; }
+    public DbSet<BlogPostEntity> BlogPosts { get; set; }
+    public DbSet<MarketplaceEntity> MarketplaceListings { get; set; }
+    public DbSet<VolunteerEntity> VolunteerOpportunities { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=pawmatedb;Username=postgres;Password=postgres");
+        }
+    }
+}
