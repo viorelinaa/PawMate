554cbcce97e7279745a645402e85d06c4ec37c2d
Valerica <valeriamandis2019@gmail.com>
2026-03-31 12:43:25 +0300

    entitate1

---
+++ PawMate.Domain/Entities/Sitter/SitterEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace PawMate.Domain.Entities.Sitter;
+
+public class SitterEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string City { get; set; }
+    public string Services { get; set; }
+    public decimal PricePerDay { get; set; }
+    public string Description { get; set; }
+}
---
+++ PawMate.Domain/Models/Sitter/SitterCreateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PawMate.Domain.Models.Sitter;
+
+public class SitterCreateDto
+{
+    public string Name { get; set; }
+    public string City { get; set; }
+    public string Services { get; set; }
+    public decimal PricePerDay { get; set; }
+    public string Description { get; set; }
+}
