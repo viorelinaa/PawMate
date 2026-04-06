9498a302ae5e5ed9ba9e98966ee9553dc4c7833f
Valerica <valeriamandis2019@gmail.com>
2026-04-07 01:11:34 +0300

    adaugare profil #1

---
+++ PawMate.Domain/Models/User/UserProfileUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PawMate.Domain.Models.User;
+
+public class UserProfileUpdateDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string Bio { get; set; } = string.Empty;
+}
