71aee812a3320aa765593105ffdc034572d0efe9
Valerica <valeriamandis2019@gmail.com>
2026-04-06 19:52:29 +0300

    modele pentru sitters

--- PawMate.Domain/Models/Sitter/SitterInfoDto.cs
+++ PawMate.Domain/Models/Sitter/SitterInfoDto.cs
@@ -3,9 +3,10 @@
 public class SitterInfoDto
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string City { get; set; }
-    public string Services { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string Services { get; set; } = string.Empty;
     public decimal PricePerDay { get; set; }
-    public string Description { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public decimal Rating { get; set; }
 }
